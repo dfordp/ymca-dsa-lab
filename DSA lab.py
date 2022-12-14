335aae69174477ddf1f9a3d9d256b16d70fbab9c
Dilpreet <92905896+dfordp@users.noreply.github.com>
2022-12-14 21:25:59 +0530

    Update DSA lab.py

--- DSA lab.py
+++ DSA lab.py
@@ -184,3 +184,58 @@ print(stack.pop())
 
 print('\nStack after elements are popped:')
 print(stack)
+
+
+#Write a program to convert Infix expression into Postfix expression and also analyze its Complexity.
+
+Operators = set(['+', '-', '*', '/', '(', ')', '^'])  # collection of Operators
+
+Priority = {'+':1, '-':1, '*':2, '/':2, '^':3} # dictionary having priorities of Operators
+ 
+ 
+def infixToPostfix(expression): 
+
+    stack = [] # initialization of empty stack
+
+    output = '' 
+
+    
+
+    for character in expression:
+
+        if character not in Operators:  # if an operand append in postfix expression
+
+            output+= character
+
+        elif character=='(':  # else Operators push onto stack
+
+            stack.append('(')
+
+        elif character==')':
+
+            while stack and stack[-1]!= '(':
+
+                output+=stack.pop()
+
+            stack.pop()
+
+        else: 
+
+            while stack and stack[-1]!='(' and Priority[character]<=Priority[stack[-1]]:
+
+                output+=stack.pop()
+
+            stack.append(character)
+
+    while stack:
+
+        output+=stack.pop()
+
+    return output
+
+
+expression = input('Enter infix expression ')
+
+print('infix notation: ',expression)
+
+print('postfix notation: ',infixToPostfix(expression))
