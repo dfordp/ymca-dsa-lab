452bdd9e0bc7aeacab7c5430a5f4d6c4d025d1e7
Dilpreet <92905896+dfordp@users.noreply.github.com>
2022-12-15 09:37:40 +0530

    Update DSA lab.py

--- DSA lab.py
+++ DSA lab.py
@@ -357,3 +357,70 @@ if __name__ == '__main__':
     q.queueDisplay()
     q.queueFront()
 
+#Write a program to implement Circular Queue using arrays
+# (Show all the operations like insertion, deletion and display)	
+
+
+class CircularQueue:
+    def __init__(self,c):
+        self.size=c
+        self.queue = [None for i in range(c)]
+        self.front=self.rear=-1
+        
+    def enqueue(self,data):
+         if ((self.rear + 1) % self.size == self.front):
+            print("\Queue is Full")
+         elif (self.front == -1):
+            self.front = 0
+            self.rear = 0
+            self.queue[self.rear] = data
+         else:
+            self.rear = (self.rear + 1) % self.size
+            self.queue[self.rear] = data
+
+    def dequeue(self):
+        if (self.front == -1):
+            print("\nQueue is empty")
+        elif (self.front == self.rear):
+            temp=self.queue[self.front]
+            self.front = -1
+            self.rear = -1
+            return temp
+        else:
+            temp = self.queue[self.front]
+            self.front = (self.front + 1) % self.size
+            return temp
+    def display(self):
+        if(self.front == -1):
+            print ("Queue is Empty")
+        elif (self.rear >= self.front):
+            print("Elements in the circular queue are:",
+                                              end = " ")
+            for i in range(self.front, self.rear + 1):
+                print(self.queue[i], end = " ")
+            print ()
+        else:
+            print ("Elements in Circular Queue are:",
+                                           end = " ")
+            for i in range(self.front, self.size):
+                print(self.queue[i], end = " ")
+            for i in range(0, self.rear + 1):
+                print(self.queue[i], end = " ")
+            print ()
+        if ((self.rear + 1) % self.size == self.front):
+            print("Queue is Full")
+
+if __name__ == '__main__':
+    ob = CircularQueue(5)
+    ob.enqueue(14)
+    ob.enqueue(22)
+    ob.enqueue(13)
+    ob.enqueue(-6)
+    ob.display()
+    print ("Deleted value = ", ob.dequeue())
+    print ("Deleted value = ", ob.dequeue())
+    ob.display()
+    ob.enqueue(9)
+    ob.enqueue(20)
+    ob.enqueue(5)
+    ob.display()
