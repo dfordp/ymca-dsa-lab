6b1b7133a906ddafbdb038a8e4eb504699de7060
Dilpreet <92905896+dfordp@users.noreply.github.com>
2022-12-16 17:13:52 +0530

    Update DSA lab.py

--- DSA lab.py
+++ DSA lab.py
@@ -469,3 +469,76 @@ list.AtBegining("Sun")
 list.AtEnd("Thu")
 list.Inbetween(list.headNode.next,"Fri")
 list.listPrint()
+
+#Implement Doubly Linked List
+
+class Node:
+    def __init__(self,data):
+        self.prev=None
+        self.data=data
+        self.next=None
+
+class DoublyLinkedList:
+    def __init__(self):
+        self.head=None
+    def printList(self,node):
+        print("\nTraversal in forward direction")
+        while node:
+            print("{}".format(node.data), end =" ")
+            last = node
+            node = node.next
+ 
+        print("\nTraversal in reverse direction")
+        while last:
+            print("{}".format(last.data), end =" ")
+            last = last.prev
+
+    def append(self, new_data): 
+        new_node = Node(new_data)
+        if self.head is None:
+            self.head = new_node
+            return
+        last = self.head
+        while last.next:
+            last = last.next
+        last.next = new_node
+        new_node.prev = last
+        return
+    def push(self, new_data):
+         new_node = Node(new_data)
+         new_node.next = self.head
+         if self.head is not None:
+            self.head.prev = new_node
+         self.head = new_node
+    def insertAfter(self, prev_node, new_data):
+         if prev_node is None:
+            print("the given previous node cannot be NULL")
+            return
+         new_node = Node(new_data)
+         new_node.next = prev_node.next
+         prev_node.next = new_node
+         new_node.prev = prev_node
+         if new_node.next:
+            new_node.next.prev = new_node
+llist = DoublyLinkedList()
+ 
+  # Insert 6. So the list becomes 6->None
+llist.append(6)
+ 
+  # Insert 7 at the beginning.
+  # So linked list becomes 7->6->None
+llist.push(7)
+ 
+  # Insert 1 at the beginning.
+  # So linked list becomes 1->7->6->None
+llist.push(1)
+ 
+  # Insert 4 at the end.
+  # So linked list becomes 1->7->6->4->None
+llist.append(4)
+ 
+  # Insert 8, after 7.
+  # So linked list becomes 1->7->8->6->4->None
+llist.insertAfter(llist.head.next, 8) 
+print("Created DLL is: ", end =" ")
+llist.printList(llist.head)
