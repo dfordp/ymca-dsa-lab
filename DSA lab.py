442f21ee7ad2165b0a92b5714edac58a713c3cd9
Dilpreet <92905896+dfordp@users.noreply.github.com>
2023-01-15 00:13:05 +0530

    Update DSA lab.py

--- DSA lab.py
+++ DSA lab.py
@@ -729,3 +729,60 @@ last = llist.addEnd(8)
 last = llist.addEnd(12)
 last = llist.addAfter(10, 8)
 llist.traverse()
+
+
+#Write a program to implement Binary Search Tree and 
+#its operations like insertion, deletion and searching.
+
+class Node:
+    left=None
+    val=0
+    right=None
+
+    def __init__(self,val) -> None:
+        self.val=val
+
+class BST:
+    root =None
+    
+    def insert(self,key):
+        node = Node(key)
+        if (self.root == None):
+            self.root = node
+            return
+        prev = None
+        temp = self.root
+        while (temp != None):
+            if (temp.val > key):
+                prev = temp
+                temp = temp.left
+            elif(temp.val < key):
+                prev = temp
+                temp = temp.right
+        if (prev.val > key):
+            prev.left = node
+        else:
+            prev.right = node
+
+    def inorder(self):
+        temp = self.root
+        stack = []
+        while (temp != None or not (len(stack) == 0)):
+            if (temp != None):
+                stack.append(temp)
+                temp = temp.left
+            else:
+                temp = stack.pop()
+                print(str(temp.val) + " ", end="")
+                temp = temp.right
+ 
+
+tree = BST()
+tree.insert(30)
+tree.insert(50)
+tree.insert(15)
+tree.insert(20)
+tree.insert(10)
+tree.insert(40)
+tree.insert(60)
+tree.inorder()
